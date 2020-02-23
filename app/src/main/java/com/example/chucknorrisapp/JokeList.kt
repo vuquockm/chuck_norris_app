30aab4588152ef8933665e74d567e3a2477fe51b
Marc Vu <marc.vuquockhanh@edu.esiee.fr>
2020-02-23 17:59:20 +0100

    Jokes list and main activity

---
+++ app/src/main/java/com/example/chucknorrisapp/JokeList.kt
@@ -0,0 +1,26 @@
+package com.example.chucknorrisapp
+
+object JokeList {
+    var jokes = listOf("Chuck Norris went out of an infinite loop.",
+        "Chuck Norris doesn't use a computer because a computer does " +
+        "everything slower than Chuck Norris.",
+        "Tom Clancy has to pay royalties to Chuck Norris because " +
+        "'The Sum of All Fears' is the name of Chuck Norris' autobiography.",
+        "Most people fear the Reaper. Chuck Norris considers him 'a promising Rookie'.",
+        "The crossing lights in Chuck Norris's home town say 'Die slowly' and 'die quickly'. " +
+        "They each have a picture of Chuck Norris punching or kicking a pedestrian.",
+        "How many Chuck Norris' does it take to change a light bulb? " +
+        "None, Chuck Norris prefers to kill in the dark.",
+        "Chuck Norris wears a live rattlesnake as a condom.",
+        "Chuck Norris built the hospital he was born in.",
+        "On the set of Walker Texas Ranger Chuck Norris brought a dying lamb back " +
+        "to life by nuzzling it with his beard. As the onlookers gathered, the lamb " +
+        "sprang to life. Chuck Norris then roundhouse kicked it, killing it instantly. " +
+        "This was just to prove that the good Chuck givet.",
+        "A movie scene depicting Chuck Norris losing a fight with Bruce Lee was the " +
+        "product of history's most expensive visual effect. When adjusted for inflation, " +
+        "the effect cost more than the Gross National Product of Paraguay."
+    )
+
+
+}
