8ad5807b2e5773fb8de8d9852eea200a524016b1
vuquockm <marc.vuquockhanh@edu.esiee.fr>
2020-03-19 17:35:32 +0100

    Leaks Killer 2.5 Added CompositeDisponible() Overidd onStop() function

--- app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
@@ -5,30 +5,41 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
+    private val disposables = CompositeDisposable()
 
-    @SuppressLint("CheckResult")
+    //@SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         Log.d("sorted", JokeList.jokes.toString())
 
         rvjokes.layoutManager = LinearLayoutManager(this)
         val jokeService= JokeApiServiceFactory.service()
-        jokeService.giveMeAJoke().subscribeOn(Schedulers.io()).subscribeBy(
+        disposables.add( jokeService.giveMeAJoke().subscribeOn(Schedulers.io()).subscribeBy(
             onSuccess = {Log.d("Trump", it.toString())},
             onError = {Log.e("Warren", "fail",it)}
-        )
+        ))
         val jokeList = JokeList.jokes.toJokes()
         val adapter=JokeAdapter()
         adapter.jokeList = jokeList
         rvjokes.adapter=adapter
+
     }
+
+    override fun onStop() {
+        disposables.clear()
+        super.onStop()
+
+    }
+
 
     //D’abord, comment transformer un String en Joke de test à l’aide d’une extension ?
     fun String.toJoke() = Joke(createdAt = "test", iconUrl = "test",updatedAt = "test",url = "test",id = "test",value=this)
