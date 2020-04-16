81d44af7f1afcff4bfa294f99a5b2f1821365f30
vuquockm <marc.vuquockhanh@edu.esiee.fr>
2020-04-16 21:50:35 +0200

    4. Save jokes Added save joke and unsave Joke functions, to save joke and remove jokes from Shared Preferences. Changed Data Model to contain a joke. Init favicon depending on Joke saved property.

--- app/src/main/java/com/example/chucknorrisapp/JokeView.kt
+++ app/src/main/java/com/example/chucknorrisapp/JokeView.kt
@@ -2,19 +2,17 @@ package com.example.chucknorrisapp
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.util.AttributeSet
 import android.util.Log
-import android.view.InflateException
 import android.view.LayoutInflater
-import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat.startActivity
-import androidx.core.view.marginLeft
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.joke_layout.view.*
+import kotlinx.serialization.builtins.list
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+
 
 class JokeView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -31,15 +29,19 @@ class JokeView @JvmOverloads constructor(
     }
 
 
-    data class Model(val text:String,val shareIcon:Int,val favoriIcon:Int,val id:String)
+    //data class Model(val text:String,val shareIcon:Int,val favoriIcon:Int,val id:String)
+    data class Model(val joke: Joke)
 
     fun setupView(model: Model){
+        textview.text=model.joke.value
+        btnstate=if(model.joke.saved) R.drawable.ic_star_black_24dp else R.drawable.ic_star_border_black_24dp
+        btnfav.setImageResource(btnstate)
         //inflate(context,R.layout.joke_layout,this)
         btnshare.setOnClickListener{
-        Log.d("Gbaman", model.id)
+        Log.d("Gbaman", model.joke.id)
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, model.text)
+                putExtra(Intent.EXTRA_TEXT, model.joke.value)
                 type = "text/plain"
             }
 
@@ -47,12 +49,54 @@ class JokeView @JvmOverloads constructor(
             startActivity(context,shareIntent,null)
         }
         btnfav.setOnClickListener{
-        Log.d("Kemar",model.id)
-            btnstate=if(btnstate==R.drawable.ic_star_border_black_24dp)R.drawable.ic_star_black_24dp else R.drawable.ic_star_border_black_24dp
+            Log.d("Kemar",model.joke.id) //log joke id
+            btnstate = if(!model.joke.saved){
+                saveJoke(model.joke)
+                R.drawable.ic_star_black_24dp
+            } else{
+                unsaveJoke(model.joke)
+                R.drawable.ic_star_border_black_24dp
+            }
             btnfav.setImageResource(btnstate)
 
+
         }
-        textview.text=model.text
+
 
     }
+
+    fun saveJoke(joke: Joke){
+        joke.saved=true
+        // get shared preferences
+        val pref: SharedPreferences = context.getSharedPreferences("jokePref", 0) // 0 - for private mode
+        val editor: SharedPreferences.Editor = pref.edit()
+        var strJokes=pref.getString("jokes", null)
+        //if strJokes is null create jokeList
+        var jokes :MutableList<Joke> =  mutableListOf()
+        if (strJokes != null){
+            jokes = Json(JsonConfiguration.Stable).parse(Joke.serializer().list, strJokes.toString()) as MutableList<Joke>
+        }
+        jokes.add(joke)
+        strJokes = Json(JsonConfiguration.Stable).stringify<MutableList<Joke>>(Joke.serializer().list, jokes)
+        editor.putString("jokes",strJokes)
+        editor.apply()
+    }
+
+    fun unsaveJoke(joke: Joke){
+        joke.saved=false
+        // get shared preferences
+        val pref: SharedPreferences = context.getSharedPreferences("jokePref", 0) // 0 - for private mode
+        val editor: SharedPreferences.Editor = pref.edit()
+        var strJokes=pref.getString("jokes", null)
+        val jokes = Json(JsonConfiguration.Stable).parse(Joke.serializer().list, strJokes.toString()) as MutableList<Joke>
+        // get index of joke
+        val index = jokes.indexOfFirst{
+            it.id == joke.id
+        }
+        jokes.removeAt(index)
+        strJokes = Json(JsonConfiguration.Stable).stringify<MutableList<Joke>>(Joke.serializer().list, jokes)
+        editor.putString("jokes",strJokes)
+        editor.apply()
+    }
+
 }
