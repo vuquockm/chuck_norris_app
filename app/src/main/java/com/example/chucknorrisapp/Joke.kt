eca46f031b5bb3d497382b027af2963e59763ba7
vuquockm <marc.vuquockhanh@edu.esiee.fr>
2020-04-16 21:45:47 +0200

    4. Save jokes Added saved property to know if joke comes from Shared Preferences.

--- app/src/main/java/com/example/chucknorrisapp/Joke.kt
+++ app/src/main/java/com/example/chucknorrisapp/Joke.kt
@@ -15,5 +15,6 @@ data class Joke(
     @SerialName("updated_at")
     var updatedAt: String = "2020-01-05 13:42:29.296379",
     var url: String = "https://api.chucknorris.io/jokes/p3GHoau2SLGTv04XhtIeeg",
-    var value: String = "Which came first - the chicken or the egg? Chuck Norris said it was the beer which came first, the hell with these damn animals!!!"
+    var value: String = "Which came first - the chicken or the egg? Chuck Norris said it was the beer which came first, the hell with these damn animals!!!",
+    var saved:Boolean=false
 )
